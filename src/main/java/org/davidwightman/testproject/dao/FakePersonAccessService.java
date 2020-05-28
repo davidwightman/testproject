e360ed97dc2754d7eb8b0d62affaef7555bd84a7
David Wightman <david@davidwightman.org>
2020-05-28 08:27:34 -0400

    built POST api endpoint for Person

--- src/main/java/org/davidwightman/testproject/dao/FakePersonAccessService.java
+++ src/main/java/org/davidwightman/testproject/dao/FakePersonAccessService.java
@@ -2,12 +2,20 @@ package org.davidwightman.testproject.dao;
 
 
 import org.davidwightman.testproject.model.Person;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonAccessService implements PersonDao {
+
+    private static List<Person> DB = new ArrayList<>();
+
     @Override
     public int insertPerson(UUID id, Person person) {
-        return 0;
+        DB.add(new Person(id, person.getName()));
+        return 1;
     }
 }
---
+++ src/main/java/org/davidwightman/testproject/service/PersonService.java
@@ -0,0 +1,23 @@
+package org.davidwightman.testproject.service;
+
+import org.davidwightman.testproject.dao.PersonDao;
+import org.davidwightman.testproject.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao")PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+}
